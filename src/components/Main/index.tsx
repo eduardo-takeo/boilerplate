3c46a8f93bfba8d92547f56634b9ac384db7c0fb
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2022-03-19 16:03:43 -0300

    Create main component and add jest-styled-components to improve snapshots

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,17 @@
+import * as S from './styles'
+
 export default function Main() {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo src="/img/logo.svg" alt="Imagem de um átomo e React avançado" />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled-components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para a tela com código"
+      />
+    </S.Wrapper>
   )
 }
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -10,4 +10,10 @@ describe('<Main />', () => {
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
   })
+
+  it('should render correct colors', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })

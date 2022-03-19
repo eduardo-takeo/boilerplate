1478adfe8b0918ce4bd75c95505930d1fc8834ff
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2022-03-19 15:13:37 -0300

    Create global styles and remove unnecessary files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import GlobalStyle from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+        {/* <link rel="shortcut-icon" href="#" /> */}
+        {/* <link rel="apple-touch-icon" href="#" /> */}
+        <meta
+          name="description"
+          content="A simple project starter using Typescript, React, NextJS and Styled-components"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App

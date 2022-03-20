c0db1d197e08589dae38043c41bd48030c559a4f
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2022-03-19 23:16:58 -0300

    Include controls to story on storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,8 +1,13 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
-}
+  component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'Typescript, ReactJS, NextJS e Styled-components'
+  }
+} as Meta
 
-export const Basic = () => <Main />
+export const Basic: Story = (args) => <Main {...args} />

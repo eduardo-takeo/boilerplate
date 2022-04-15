0a8a81a2a5bbe78fb6befbbc687f3ba50e46cee9
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2022-04-15 15:21:38 -0300

    Add global fonts, and automate component creation

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,60 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v19-latin-300.woff2') format('woff2'),
+       url('/fonts/poppins-v19-latin-300.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v19-latin-regular.woff2') format('woff2'),
+       url('/fonts/poppins-v19-latin-regular.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v19-latin-600.woff2') format('woff2'),
+       url('/fonts/poppins-v19-latin-600.woff') format('woff');
+}
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
-  html {
-    font-size: 62.5%;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    html,
+    body,
+    #__next {
+      height: 100%;
+    }
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 `
 
 export default GlobalStyle
